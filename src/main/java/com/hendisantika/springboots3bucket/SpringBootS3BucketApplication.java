c3a004dcbe6e0b858b2718c0f87b21b97ceb144e
Hendi Santika <hendisantika@gmail.com>
2021-01-24 00:09:31 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springboots3bucket/SpringBootS3BucketApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springboots3bucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootS3BucketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootS3BucketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springboots3bucket/SpringBootS3BucketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springboots3bucket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootS3BucketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
